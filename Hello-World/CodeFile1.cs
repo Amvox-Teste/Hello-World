153543c7b1b67600730d4d39f4593b004d858749
Daylane <daylane86@gmail.com>
2022-07-12 11:03:54 -0300

    Hello-World em outras linguas

---
+++ Hello-World/CodeFile1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Mundo
+    {
+        static void Metodo(string[] args)
+        {
+            Console.WriteLine("헬로월드");// Coreano
+
+            Console.WriteLine("你好世界"); //Chinês
+        }
+    }
+}
