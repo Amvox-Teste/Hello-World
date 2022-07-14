ba5646c6e759bb2e4e25833d9f67f3219ca12f05
Daylane <daylane86@gmail.com>
2022-07-14 11:50:33 -0300

    Novo commit

--- Hello-World/Program.cs
+++ Hello-World/Program.cs
@@ -6,11 +6,11 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World!");
 
             Console.WriteLine("Olá, Mundo!");
             
-            Console.WriteLin("Olá, Hallo Welt!");
+            Console.WriteLine("Olá, Hallo Welt!");
         }
     }
 }
